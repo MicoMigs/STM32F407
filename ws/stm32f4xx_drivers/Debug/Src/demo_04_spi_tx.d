d86dfdac194e07e86c7b0e2e7bffbc1d0dc8272a
Mico Santiago <Micomiguelsantiago@gmail.com>
2025-08-19 22:02:59 -0700

    update

--- ws/stm32f4xx_drivers/Debug/Src/demo_04_spi_tx.d
+++
@@ -1,9 +0,0 @@
-Src/demo_04_spi_tx.o: ../Src/demo_04_spi_tx.c \
- C:/STM32F407/ws/stm32f4xx_drivers/Drivers/Inc/stm32f407xx.h \
- C:/STM32F407/ws/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_gpio_driver.h \
- C:/STM32F407/ws/stm32f4xx_drivers/Drivers/Inc/stm32f407xx.h \
- C:/STM32F407/ws/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_spi_driver.h
-C:/STM32F407/ws/stm32f4xx_drivers/Drivers/Inc/stm32f407xx.h:
-C:/STM32F407/ws/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_gpio_driver.h:
-C:/STM32F407/ws/stm32f4xx_drivers/Drivers/Inc/stm32f407xx.h:
-C:/STM32F407/ws/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_spi_driver.h:
---
+++ ws/stm32f4xx_drivers/Debug/Src/demo_05_spi_master_cmd.d
@@ -0,0 +1,9 @@
+Src/demo_05_spi_master_cmd.o: ../Src/demo_05_spi_master_cmd.c \
+ C:/STM32F407/ws/stm32f4xx_drivers/Drivers/Inc/stm32f407xx.h \
+ C:/STM32F407/ws/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_gpio_driver.h \
+ C:/STM32F407/ws/stm32f4xx_drivers/Drivers/Inc/stm32f407xx.h \
+ C:/STM32F407/ws/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_spi_driver.h
+C:/STM32F407/ws/stm32f4xx_drivers/Drivers/Inc/stm32f407xx.h:
+C:/STM32F407/ws/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_gpio_driver.h:
+C:/STM32F407/ws/stm32f4xx_drivers/Drivers/Inc/stm32f407xx.h:
+C:/STM32F407/ws/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_spi_driver.h:
